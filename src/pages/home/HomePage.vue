7aee83ff675c52f9feb9ffe1a9307e23384e567b
samantabarros <samantadesenvolvedora@gmail.com>
2023-10-20 09:49:10 -0300

    Propriedade hover no card aluno do menu principal

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="q-pa-md">
     <router-link to="/alunos" style="text-decoration: none">
-      <q-card
-        class="bg-blue-3 row justify-center items-center card-hovered"
-        style="width: 320px; height: 180px"
-      >
+      <div class="bg-blue-3 row justify-center items-center card-aluno" style="width: 320px; height: 180px;">
         <div class="column items-center">
           <q-icon name="groups" size="100px" color="white" />
           <p class="text-white" style="font-size: 30px">Alunos</p>
         </div>
-      </q-card>
+      </div>
     </router-link>
   </div>
 </template>
@@ -17,8 +14,11 @@
 <script setup>
 </script>
 <style scoped>
-.card-hovered:hover{
-  background-color: rgba(0,0,255,0.8)
+
+
+.card-aluno:hover {
+  box-shadow: 0px 0px 20px 2px #c96f153f;
+  transform: scale(1.05);
 }
 
 </style>
