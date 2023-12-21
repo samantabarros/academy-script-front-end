57a722f2da54a7473d74eb031807747a5535c6e0
Samanta Barros <samantadesenvolvedora@gmail.com>
2023-12-21 13:45:30 -0300

    Reposicionando os cards do menu

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="row justify-center menu-cards">
+  <div class="row flex justify-center items-center menu-cards">
     <div class="q-pa-md">
       <router-link to="/alunos" style="text-decoration: none">
         <div
           class="bg-blue-6 row justify-center items-center card-aluno"
-          style="width: 400px; height: 400px"
+          style="width: 400px; height: 300px"
         >
           <div class="column items-center">
             <q-icon name="groups" size="100px" color="white" />
@@ -17,7 +17,7 @@
       <router-link to="/modulos" style="text-decoration: none">
         <div
           class="bg-deep-purple-6 row justify-center items-center card-aluno"
-          style="width: 400px; height: 400px"
+          style="width: 400px; height: 300px"
         >
           <div class="column items-center">
             <q-icon name="auto_stories" size="100px" color="white" />
@@ -35,7 +35,11 @@
   transform: scale(1.05);
 }
 
-.menu-cards{
-  margin-top: 100px;
+.card-aluno{
+  transition: transform .4s;
+}
+
+.menu-cards {
+  min-height: calc(100vh - 50px - 42px);
 }
 </style>
